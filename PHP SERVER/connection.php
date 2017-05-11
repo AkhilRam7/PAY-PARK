133b4c700fcd18de63eb4e69c09a972a2474ae90
AkhilRam7 <g.akhilram@gmail.com>
2017-05-12 00:48:59 +0530

    Update connection.php

--- PHP SERVER/connection.php
+++ PHP SERVER/connection.php
@@ -1,11 +1,1 @@
-<?php
 
-define('hostname', 'localhost');
-define('user', 'root');
-define('password', 'akhil');
-define('databaseName', 'parking');
-
-
-$connect = mysqli_connect(hostname, user, password, databaseName);
-
-?>
